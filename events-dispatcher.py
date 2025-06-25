a0bb87b1c8da95d87f478513b35e02dba99e63e1
mat <matteo.gattanini@gmail.com>
2025-06-25 11:09:37 +0200

    feat: step-data argument for job-selected event

--- events-dispatcher.py
+++ events-dispatcher.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import socketserver, json
+import socketserver, json, re
 from datetime import datetime, timezone
 try: import redis
 except ImportError: redis=None
@@ -18,20 +18,48 @@ redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB) if red
 if not redis_client:
     print("! Redis client not available, install it with: > pip install redis")
 
+def csv_to_intarray(csv_indexes):
+    try:
+        return [int(x) for x in csv_indexes.split(",") if x.strip()]
+    except ValueError:
+        return csv_indexes
+
+def mac_to_object(bad_json_string):
+    def parse_my_bad_json(s: str) -> str:
+        parsed_object = {}
+        pattern = re.compile(r"(\w+)\s*:\s*\{([^}]+)\}")
+        for key, body in pattern.findall(s):
+            sub_dict = {}
+            items = re.split(r'[,\s]+', body.strip())
+            for item in items:
+                if ":" in item:
+                    subkey,subval = item.split(":", 1)
+                    try:
+                        sub_dict[subkey] = int(subval)
+                    except ValueError:
+                        try:
+                            sub_dict[subkey] = float(subval)
+                        except ValueError:
+                            sub_dict[subkey] = subval
+                else:
+                    sub_dict[item] = True
+            parsed_object[key] = sub_dict
+        return parsed_object
+    return parse_my_bad_json(bad_json_string)
+
 def convert_json_payload(original_json):
     """
     Convert:
     {'timestamp': '2025-06-05T08:46:11+02:00',
-     'event': 'step-started',
-     'args': {'prj-name': 'test-prj', 'scheme': 1, 'step': 2}}
+     'event': 'eventname',
+     'args': {'arg1': 'str', 'arg2': 2}}
 
     To:
     {'timestamp': '2025-06-05T06:46:11Z',
       'deviceData': [
-        {'Id': 'event', 'val': 'step-started'},
-        {'Id': 'prj-name', 'val': 'test-prj'},
-        {'Id': 'scheme', 'val': 1},
-        {'Id': 'step', 'val': 2}
+        {'Id': 'event', 'val': 'eventname'},
+        {'Id': 'arg1', 'val': 'str'},
+        {'Id': 'arg2', 'val': 2},
       ]}
     """
     converted_json = {'timestamp':'', "deviceData":[]}
@@ -45,31 +73,23 @@ def convert_json_payload(original_json):
     #if "args" in original_json:
     #    converted_json["deviceData"].extend({"Id": key, "val": val} for key, val in original_json["args"].items())
 
-    def csv_to_intarray(csv_indexes):
-        try:
-            return [int(x) for x in csv_indexes.split(",") if x.strip()]
-        except ValueError:
-            return csv_indexes
-
     if "args" in original_json:
         for key, val in original_json["args"].items():
             if key=="emg-list" and isinstance(val, str):
                 converted_json["deviceData"].append({"Id": key, "val": csv_to_intarray(val)})
+            elif key=="step-data" and isinstance(val, str):
+                converted_json["deviceData"].append({"Id": key, "val": mac_to_object(val)}) # json.dumps()
             else:
                 converted_json["deviceData"].append({"Id": key, "val": val})
     return converted_json
 
 def handle_json_payload(client_ip, client_port, json_data):
-    print_incoming_payload(client_ip, client_port, json_data)
     if redis_client:
         try:
             msg = json.dumps(json_data, separators=(",", ":"))
             redis_client.publish(REDIS_CHANNEL, msg)
         except Exception as e:
             print(f"!! Failed to publish to Redis: {e}")
-
-def print_incoming_payload(client_ip, client_port, incoming_data):
-    print(f"\n[from {client_ip}:{client_port} at {datetime.now().time()}]\n{incoming_data}")
 
 class JsonTcpHandler(socketserver.StreamRequestHandler):
     def handle(self):
@@ -80,15 +100,16 @@ class JsonTcpHandler(socketserver.StreamRequestHandler):
         while True:
             line = self.rfile.readline()
             if not line:
-                print(f"\nClient {client_ip}:{client_port} disconnected")
+                print(f"\n[{client_ip}:{client_port} disconnected]")
                 break
             try:
                 incoming_str = line.decode("utf8").strip()
+                print(f"\n[from {client_ip}:{client_port} at {datetime.now().time()}]\n{incoming_str}")
                 incoming_json = json.loads(incoming_str)
                 converted_json = convert_json_payload(incoming_json)
+                print(converted_json)
                 handle_json_payload(client_ip, client_port, converted_json)
             except json.JSONDecodeError as e:
-                print_incoming_payload(client_ip, client_port, incoming_str)
                 print(f"! Invalid json at {line!r}\nError: {e}")
 
 if __name__ == '__main__':
--- test-send-events.py
+++ test-send-events.py
@@ -10,22 +10,25 @@ PORT = 9999
 def get_timestamp_tz():
     return datetime.now().astimezone().isoformat(timespec='milliseconds')
 
-def send_data(client_socket, data):
-    json_str = json.dumps(data) + "\n"
-    client_socket.sendall(json_str.encode("utf-8"))
+def send_object(client_socket, obj):
+    send_json_str(client_socket, f"{json.dumps(obj)}\n")
+
+def send_json_str(client_socket, json_str):
+    json_str = json_str.encode("utf-8")
+    client_socket.sendall(json_str)
     print(f"Sent: {json_str}")
 
 if __name__ == '__main__':
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((HOST, PORT))
-        send_data(client_socket, { 'timestamp':get_timestamp_tz(),
-                                   'event':'status-changed',
-                                   'args':{'generic-status':'WORK'} })
+        send_object(client_socket, {'timestamp':get_timestamp_tz(),
+                                    'event':'status-changed',
+                                    'args':{'generic-status':'WORK'} })
         time.sleep(1)
-        send_data(client_socket, { 'timestamp':get_timestamp_tz(),
-                                   'event':'step-started',
-                                   'args':{'prj-name':'test-prj', 'scheme':1, 'step':2} })
+        send_object(client_socket, {'timestamp':get_timestamp_tz(),
+                                    'event':'step-started',
+                                    'args':{'prj-name':'test-prj', 'scheme':1, 'step':2, 'step-data':'status:{stack:0, proc:1}, sheet:{size:3210x2400, align:2447},  op:{lowe, score, y:500|1600}, prod:{size:2447x2400, rotate:-90}, remn:{size:763x2400}'} })
         time.sleep(1)
-        send_data(client_socket, { 'timestamp':get_timestamp_tz(),
-                                   'event':'alarms-changed',
-                                   'args':{'emg-list':'123,30800012'} })
+        send_object(client_socket, {'timestamp':get_timestamp_tz(),
+                                    'event':'alarms-changed',
+                                    'args':{'emg-list':'123,30800012'} })
